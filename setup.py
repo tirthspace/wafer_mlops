951093481e3dbe0d36e4a88d0da11c34bb05c8cb
tirthspace <tirthspace@gmail.com>
2021-03-23 09:08:50 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predicting whether a wafer needs to be replaced or not based on input from various sensors',
+    author='sindhu_tirth',
+    license='MIT',
+)
